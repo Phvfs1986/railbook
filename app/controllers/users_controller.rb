ee7612f71541b192f07bb56c499c4662941e2800
Phvfs1986 <phvfs19@gmail.com>
2023-06-28 17:29:53 -0300

    add friend request notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @friendships = current_user.friendships
-    @friends = @friendships.select(&:mutual?).map(&:user).uniq
+    @friends = @friendships.select(&:mutual?).map(&:user).map(&:profile)
   end
 
   def create_request
-    current_user.friends << User.find(params[:id])
+    User.find(params[:id]).friends << current_user
 
     redirect_to root_path
+  end
+
+  def friend_list
+    @requests = current_user.pending_friends
+    @friends = current_user.active_friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'users/create_request/:id', to: 'users#create_request', as: 'user_create_request'
+  get 'users/friend_list/:id', to: 'users#friend_list', as: 'user_friend_list'
   resources :profiles, only: %i[show edit update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
